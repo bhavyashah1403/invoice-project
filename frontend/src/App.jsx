ec8f06addcc548a6e17077d2b228d1fc45752b79
bhavyashah1403 <bhavya1403@gmail.com>
2026-04-22 22:12:44 +0530

    update App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import "./index.css";
 import { numberToWords } from "./utils/numberToWords";
-import { capitalizeFirstAndLastName } from "./utils/capitalizeNames";
 import { paymentModes, purposes, indianBanks } from "./utils/dropdownData";
 import { SearchableDropdown } from "./components/SearchableDropdown";
 
@@ -55,11 +54,6 @@ export default function App() {
   const change = (e) => {
     const { name, value } = e.target;
     const updatedForm = { ...form, [name]: value };
-    
-    // Auto-capitalize first and last letter of patient name
-    if (name === "customer_name" && value) {
-      updatedForm.customer_name = capitalizeFirstAndLastName(value);
-    }
     
     // Auto-convert amount to words when amount field changes
     if (name === "amount" && value) {
